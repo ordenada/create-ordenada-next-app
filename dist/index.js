b7f8039d7813ab56f803da9a5e6d2c7a9f36abcd
David256 <wasterdavid@gmail.com>
2025-02-15 23:28:21 -0500

    revert: "feat: update tsconfig for commonjs"
    This reverts commit aead9212de42de1a0adb7199aaa506c3b5a61660.

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,9 @@
 #!/usr/bin/env node
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const path_1 = __importDefault(require("path"));
-const fs_1 = __importDefault(require("fs"));
-const child_process_1 = require("child_process");
-const promises_1 = __importDefault(require("readline/promises"));
-const reader = promises_1.default.createInterface({
+import path from 'path';
+import fs from 'fs';
+import { execSync } from 'child_process';
+import readline from 'readline/promises';
+const reader = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
@@ -23,21 +18,21 @@ async function main() {
     /**
      * Get the app path
      */
-    const appPath = path_1.default.isAbsolute(appName)
+    const appPath = path.isAbsolute(appName)
         ? appName
-        : path_1.default.join(process.cwd(), appName);
-    if (fs_1.default.existsSync(appPath) && fs_1.default.readdirSync(appPath).length > 0) {
+        : path.join(process.cwd(), appName);
+    if (fs.existsSync(appPath) && fs.readdirSync(appPath).length > 0) {
         console.error('Error: the folder is not empty.');
         process.exit(1);
     }
-    appName = path_1.default.basename(appPath);
+    appName = path.basename(appPath);
     console.log(`creating the app "${appName}" (${appPath})`);
-    process.chdir(path_1.default.dirname(appPath));
+    process.chdir(path.dirname(appPath));
     /**
      * Create the Next.js app
      */
     console.log('creating next.js app...');
-    (0, child_process_1.execSync)(`bunx create-next-app@latest --typescript --tailwind --eslint --app --src-dir --turbopack --import-alias "@/*" ${appPath}`, {
+    execSync(`bunx create-next-app@latest --typescript --tailwind --eslint --app --src-dir --turbopack --import-alias "@/*" ${appPath}`, {
         stdio: 'inherit',
     });
     // Go in
@@ -46,20 +41,20 @@ async function main() {
      * Prepare cypress
      */
     console.log('installing cypress...');
-    (0, child_process_1.execSync)('bun add -d cypress');
+    execSync('bun add -d cypress');
     console.log('config cypress scripts');
-    const packageJsonPath = path_1.default.join(appPath, 'package.json');
-    const packageJson = JSON.parse(fs_1.default.readFileSync(packageJsonPath, 'utf8'));
+    const packageJsonPath = path.join(appPath, 'package.json');
+    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
     packageJson.scripts = {
         ...packageJson.scripts,
         'cy:open': 'cypress open',
     };
-    fs_1.default.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
     /**
      * Prepare storybook
      */
     console.log('installing storybook');
-    (0, child_process_1.execSync)('bunx storybook@latest init --no-dev');
+    execSync('bunx storybook@latest init --no-dev');
 }
 main().finally(() => {
     process.exit();
