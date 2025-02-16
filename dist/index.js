1096fa163d6bb22a3dce3d0089e15a2a4edcbfa0
David256 <wasterdavid@gmail.com>
2025-02-15 21:55:36 -0500

    chore: update dist files

--- dist/index.js
+++ dist/index.js
@@ -1,56 +1,65 @@
-import path from 'path';
-import fs from 'fs';
-import { execSync } from 'child_process';
-import readline from 'readline/promises';
-const reader = readline.createInterface({
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
+const child_process_1 = require("child_process");
+const promises_1 = __importDefault(require("readline/promises"));
+const reader = promises_1.default.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
-/**
- * Get the app name
- */
-let appName = process.argv[2];
-if (!appName) {
-    appName = await reader.question('package name: ');
+async function main() {
+    /**
+     * Get the app name
+     */
+    let appName = process.argv[2];
+    if (!appName) {
+        appName = await reader.question('package name: ');
+    }
+    /**
+     * Get the app path
+     */
+    const appPath = path_1.default.isAbsolute(appName)
+        ? appName
+        : path_1.default.join(process.cwd(), appName);
+    if (fs_1.default.existsSync(appPath) && fs_1.default.readdirSync(appPath).length > 0) {
+        console.error('Error: the folder is not empty.');
+        process.exit(1);
+    }
+    appName = path_1.default.basename(appPath);
+    console.log(`creating the app "${appName}" (${appPath})`);
+    process.chdir(path_1.default.dirname(appPath));
+    /**
+     * Create the Next.js app
+     */
+    console.log('creating next.js app...');
+    (0, child_process_1.execSync)(`bunx create-next-app@latest --typescript --tailwind --eslint --app --src-dir --turbopack --import-alias "@/*" ${appPath}`, {
+        stdio: 'inherit',
+    });
+    // Go in
+    process.chdir(appPath);
+    /**
+     * Prepare cypress
+     */
+    console.log('installing cypress...');
+    (0, child_process_1.execSync)('bun add -d cypress');
+    console.log('config cypress scripts');
+    const packageJsonPath = path_1.default.join(appPath, 'package.json');
+    const packageJson = JSON.parse(fs_1.default.readFileSync(packageJsonPath, 'utf8'));
+    packageJson.scripts = {
+        ...packageJson.scripts,
+        'cy:open': 'cypress open',
+    };
+    fs_1.default.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+    /**
+     * Prepare storybook
+     */
+    console.log('installing storybook');
+    (0, child_process_1.execSync)('bunx storybook@latest init --no-dev');
 }
-/**
- * Get the app path
- */
-const appPath = path.isAbsolute(appName)
-    ? appName
-    : path.join(process.cwd(), appName);
-if (fs.existsSync(appPath) && fs.readdirSync(appPath).length > 0) {
-    console.error('Error: the folder is not empty.');
-    process.exit(1);
-}
-appName = path.basename(appPath);
-console.log(`creating the app "${appName}" (${appPath})`);
-process.chdir(path.dirname(appPath));
-/**
- * Create the Next.js app
- */
-console.log('creating next.js app...');
-execSync(`bunx create-next-app@latest --typescript --tailwind --eslint --app --src-dir --turbopack --import-alias "@/*" ${appPath}`, {
-    stdio: 'inherit',
+main().finally(() => {
+    process.exit();
 });
-// Go in
-process.chdir(appPath);
-/**
- * Prepare cypress
- */
-console.log('installing cypress...');
-execSync('bun add -d cypress');
-console.log('config cypress scripts');
-const packageJsonPath = path.join(appPath, 'package.json');
-const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
-packageJson.scripts = {
-    ...packageJson.scripts,
-    'cy:open': 'cypress open',
-};
-fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-/**
- * Prepare storybook
- */
-console.info('installing storybook');
-execSync('bunx storybook@latest init --no-dev');
-process.exit();
